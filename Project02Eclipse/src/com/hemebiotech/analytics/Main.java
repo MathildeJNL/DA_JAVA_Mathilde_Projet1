432d4d665ae6263262d6763276b9eccea394f8c5
Mathilde Jnl <mathilde.jeannolle@gmail.com>
2023-01-27 00:54:04 +0100

    File organization

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -0,0 +1,17 @@
+package com.hemebiotech.analytics;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+//ENVIRON 8 LIGNES EN PRESENCE DANS LE MAIN (initialisation reader, counter, order, write) + méthodes associées (getSymptom, countSymptom, orderSymptom, writeResult)
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
---
+++ Project02Eclipse/src/com/hemebiotech/analytics/implement/AnalyticsCounter.java
@@ -0,0 +1,9 @@
+package com.hemebiotech.analytics.implement;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+public class AnalyticsCounter {
+	
+}
